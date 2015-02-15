aa4b78a03b7880f21b1b808962a1449fa5164a02
Tim Zallin <tim.zallin@gmail.com>
2015-02-16 01:06:25 +0400

    fixed

--- public/main.js
+++ public/main.js
@@ -20,16 +20,4 @@ game.global = {
 	tapDelay : 500
 }
 
-game.global.setSize = function(){
-		var size = Math.floor((this.height - this.offsetY * 2) / this.rows); 
-		while(size * this.cols > this.width - this.offsetX * 2){
-			size--;
-		}
-		this.offsetX = (this.width - size * this.cols) / 2;
-		this.offsetY = (this.height - size * this.rows) / 2;
-		this.size = size;
-};
-
 game.global.iconNames = ['circle', 'triangle', 'square', 'pentagon'];
-
-game.global.setSize();
