87bb9460debd6c421abbf8f5672920ef1a345f3a
Tim Zallin <tim.zallin@gmail.com>
2015-02-27 20:52:49 +0300

    modular refactoring

--- public/play.js
+++ public/play.js
@@ -1,42 +1,21 @@
 var playState = {
 	create : function(){
-		
+
 		this.setIconSize(game.global);
 
-		var iconScale = (game.global.iconSize / game.global.imageSize).toFixed(3),
-			rows = game.global.rows, 
-			cols = game.global.cols;
+		var icons = this.initIcons(game);
 
-		this.board = new this.Board(game);
+		this.board = this.initBoard(game, icons);
 
-		for(var i = 0, q = rows * cols; i < q; i++){
-			var rnd = Math.floor(Math.random() * game.global.iconNames.length),
-				x = Math.floor(game.global.offsetX + game.global.iconSize * (i % cols)),
-				y = Math.floor(game.global.offsetY + game.global.iconSize * Math.floor(i / cols)),
-				col = i % cols,
-				row = Math.floor(i / cols);
+		this.sequence = this.initSequence(game);
 
-			if(i > cols - 1){
-				if(this.board.icons[i - cols].key === game.global.iconNames[rnd]) rnd = this.getRandomName(rnd);
-			}
-			if(i % cols > 0){
-				if(this.board.icons[i - 1].key === game.global.iconNames[rnd]) rnd = this.getRandomName(rnd);
-			}
+		this.bonusLoader = this.initLoader(game);
 
-			var icon = game.add.sprite(x, y, game.global.iconNames[rnd]);
-			icon.scale.x = icon.scale.y = iconScale;
-			game.physics.arcade.enable(icon);
+		var self = this;
 
-			icon.inH = false;
-			icon.inV = false;
-			icon.processed = false;
-
-			this.board.icons[i] = icon;
-		}
-
-		this.board.createMap();
-
-		this.sequence = new this.Sequence(game);
+		this.board.onTurnFinished(function (res) {
+			self.bonusLoader.add(res.points * res.combo);
+		});
 
 		this.board.makeTurn();
 
@@ -58,8 +37,8 @@ var playState = {
 	},
 
 	insertIcon : function(index){
-		var txt = this.sequence.elements[0].key;
-		this.board.icons[index].loadTexture(txt);
+		var txt = this.sequence.getFirstKey();
+		this.board.insertIcon(index, txt);
 		this.sequence.shift();
 
 		this.board.makeTurn();
@@ -73,225 +52,313 @@ var playState = {
 		global.offsetX = (global.width - size * global.cols) / 2;
 		global.offsetY = (global.height - size * global.rows) / 2;
 		global.iconSize = size;
-	},
+	}
+}
 
-	getRandomName : function(prev){
+playState.initIcons = function (game) {
+	var iconScale = (game.global.iconSize / game.global.imageSize).toFixed(3),
+		rows = game.global.rows, 
+		cols = game.global.cols,
+		icons = [];
+
+	for(var i = 0, q = rows * cols; i < q; i++){
+		var rnd = Math.floor(Math.random() * game.global.iconNames.length),
+			x = Math.floor(game.global.offsetX + game.global.iconSize * (i % cols)),
+			y = Math.floor(game.global.offsetY + game.global.iconSize * Math.floor(i / cols)),
+			col = i % cols,
+			row = Math.floor(i / cols);
+
+		if(i > cols - 1){
+			if(icons[i - cols].key === game.global.iconNames[rnd]) rnd = getRandomName(rnd);
+		}
+		if(i % cols > 0){
+			if(icons[i - 1].key === game.global.iconNames[rnd]) rnd = getRandomName(rnd);
+		}
+
+		icons[i] = game.add.sprite(x, y, game.global.iconNames[rnd]);
+		icons[i].scale.x = icons[i].scale.y = iconScale;
+		game.physics.arcade.enable(icons[i]);
+
+		icons[i].inH = false;
+		icons[i].inV = false;
+		icons[i].processed = false;
+	}
+
+	function getRandomName (prev){
 		var rnd = Math.floor(Math.random() * game.global.iconNames.length);
-		if(rnd === prev) return this.getRandomName(rnd);
+		if(rnd === prev) return getRandomName(rnd);
 		return rnd;
 	}
+
+	return icons;
 }
 
-playState.Board = function(game){
-	this.icons = [];
-	this.cols = game.global.cols;
-	this.rows = game.global.rows;
+playState.initLoader = function () {
+	var sprite = game.add.sprite(game.global.offsetX, game.global.offsetY / 3, game.global.loaderName),
+		maxWidth = game.global.width - game.global.offsetX * 2,
+		pointsMax = game.global.initialCapacity,
+		points = 0,
+		pointSize;
 
-	this._combs = [];
-	this._toMove = {};
-	this.game = game;
+		setFraction();
 
-	this.map = {};
-}
-
-playState.Board.prototype = {
-	makeTurn : function () {
-		var self = this;
-
-		this.findThreesomes();
-
-		if(this._combs.length == 0) return;
-
-		this.computeMovements();
-
-		this.moveIcons(function () {
-			self.changeIndexes();
-			self.addIcons();
-			self.defaultValues();
-			self.makeTurn();
-		});
-	},
-
-	findThreesomes : function(){
-		for(var i = 0, l = this.icons.length; i < l; i++){
-			if (l - i > this.cols * 2){
-				this._checkComb(i, 'inV', 'down');
-			}
-			if (i % this.cols < this.cols - 2){
-				this._checkComb(i, 'inH', 'right');
-			}
+		function setFraction(){
+			pointSize = (maxWidth / pointsMax) / sprite.width;
 		}
 
-		if(this._combs.length < 1) return;
-
-		for(var i = 0; i < this._combs.length; i++){
-			for(var k = 0; k < this._combs[i].length; k++){
-				var index = this._combs[i][k];
-				this.icons[index].visible = false;
-			}
-		}
-	},
-
-	_checkComb :  function(i, prop, dir){
-		if (this.icons[i][prop]) return false;
-		var current = [],
-			hasComb = true;
-
-		current.push(i);
-
-		while(hasComb){
-			var prev = this.icons[i],
-				index = this.map[i][dir],
-				icon = this.icons[index];
-
-			if (icon.key == prev.key){
-				current.push(index);
-				if(!this.map[index][dir]) hasComb = false;
-			}
-			else {
-				hasComb = false;
-			}
-			i = index;
-		}
-
-		if(current.length > 2){
-			for(var i = 0, l = current.length; i < l; i++){
-				var index = current[i];
-				this.icons[index][prop] = true;
-			}
-			this._combs.push(current);
-		}
-	},
-
-	computeMovements : function(){
-		for(var i = 0, l = this._combs.length; i < l; i++){
-			for(var k = 0, j = this._combs[i].length; k < j; k++){
-				var index = this._combs[i][k],
-					icon = this.icons[index];
-				if ((icon.inV || icon.inH) && !icon.processed){
-					this._shiftUpper(index);
-					icon.processed = true;
+		return {
+			add : function (ps) {
+				sprite.scale.x += pointSize * ps;
+				points += ps;
+				if(points > pointsMax){
+					sprite.scale.x = 1;
+					pointsMax += 10;
+					points = 0;
+					setFraction();
 				}
 			}
 		}
-	},
+}
 
-	_shiftUpper : function(index){
-		for(var i = index - this.cols, l = index % this.cols; i >= l; i = i - this.cols){
-			var icon = this.icons[i];
-			if(!icon.visible) continue;
-			if(i in this._toMove){
-				this._toMove[i].y += icon.width;
-				this._toMove[i].index += this.cols;	
-			}
-			else{
-				this._toMove[i] = {
-					y : icon.y + icon.width,
-					index : i + this.cols
-				};
-			}
-		}
-	},
 
-	changeIndexes : function(){
-		var keys = Object.keys(this._toMove);
-		if(keys.length == 0) return;
+playState.initBoard = function (game, icons) {
+	var	cols = game.global.cols,
+		rows = game.global.rows,
+		combs = [],
+		toMove = [],
+		combo = 0,
+		points = 0,
+		map = {};
 
-		for(var i = keys.length - 1; i >= 0; i--){
-			var n = keys[i],
-				temp = this.icons[n],
-				index = this._toMove[n].index;
-
-			this.icons[n] = this.icons[index];
-			this.icons[index]  = temp;
-		}
-		this._toMove = {};
-	},
-
-	addIcons : function(){
-		for(var i = 0; i < this.icons.length; i++){
-			if(!this.icons[i].visible){
-				var rnd = Math.floor(Math.random() * this.game.global.iconNames.length),
-					txt = this.game.global.iconNames[rnd],
-					x = this.game.global.offsetX + (i % this.cols) * this.icons[i].width,
-					y = this.game.global.offsetY + Math.floor(i / this.cols) * this.icons[i].width;
-
-				this.icons[i].loadTexture(txt);
-				this.icons[i].reset(x, y); 
-			}
-		}
-	},
-
-	moveIcons : function (fn) {
-		if(Object.keys(this._toMove).length == 0) return fn();
-
-		var tweenCallback = (function (self) {
-			var completed = 0,
-				l = Object.keys(self._toMove).length;
-
-			return function () {
-				completed++;
-				if(completed == l){
-					fn();
-				}
-			}
-		}(this));
-
-		for(var i in this._toMove){
-			var t = this.game.add.tween(this.icons[i]).to({ y : this._toMove[i].y}, 1000);
-			t.onComplete.add(tweenCallback, this);
-			t.start();
-		}
-	},
-
-	defaultValues : function(){
-		for(var i = 0, l = this.icons.length; i < l; i++){
-			this.icons[i].inV = false;
-			this.icons[i].inH = false;
-			this.icons[i].processed = false;
-		}
-
-		this._combs = []; 
-	},
-
-	createMap : function () {
-		for(var i = 0, l = this.icons.length; i < l; i++){
-			var row = Math.floor(i / this.cols),
-				col = i % this.cols,
-				down = i + this.cols,
+		(function createMap () {
+			for(var i = 0, l = icons.length; i < l; i++){
+				var row = Math.floor(i / cols),
+				col = i % cols,
+				down = i + cols,
 				right = i + 1;
 
-			if(down > this.rows * this.cols) down = false;
+				if(down > rows * cols) down = false;
 
-			if(Math.floor(right / this.cols) > row) right = false;
+				if(Math.floor(right / cols) > row) right = false;
 
-			this.map[i] = {
-				right : right,
-				down : down
+				map[i] = {
+					right : right,
+					down : down
+				}
+			}
+		}());
+
+		function findThreesomes (){
+			for(var i = 0, l = icons.length; i < l; i++){
+				if (l - i > cols * 2){
+					checkComb(i, 'inV', 'down');
+				}
+				if (i % cols < cols - 2){
+					checkComb(i, 'inH', 'right');
+				}
+			}
+
+			if(combs.length < 1) return;
+
+			for(var i = 0, l = combs.length; i < l; i++){
+				for(var k = 0, j = combs[i].length; k < j; k++){
+					var index = combs[i][k];
+					icons[index].visible = false;
+					points += k + 1;
+				}
 			}
 		}
-	}
+
+		function checkComb (i, prop, dir){
+			if (icons[i][prop]) return false;
+			var current = [],
+				hasComb = true;
+
+			current.push(i);
+
+			while(hasComb){
+				var prev = icons[i],
+				index = map[i][dir],
+				icon = icons[index];
+
+				if (icon.key == prev.key){
+					current.push(index);
+					if(!map[index][dir]) hasComb = false;
+				}	
+				else {
+					hasComb = false;
+				}
+				i = index;
+			}
+
+			if(current.length > 2){
+				for(var i = 0, l = current.length; i < l; i++){
+					var index = current[i];
+					icons[index][prop] = true;
+				}
+				combs.push(current);
+			}
+		}
+
+		function computeMovements (){
+			for(var i = 0, l = combs.length; i < l; i++){
+				for(var k = 0, j = combs[i].length; k < j; k++){
+					var index = combs[i][k],
+					icon = icons[index];
+					if ((icon.inV || icon.inH) && !icon.processed){
+						shiftUpper(index);
+						icon.processed = true;
+					}
+				}
+			}
+		}
+
+		function shiftUpper (index) {
+			for(var i = index - cols, l = index % cols; i >= l; i = i - cols){
+				var icon = icons[i];
+				if(!icon.visible) continue;
+				if(i in toMove){
+					toMove[i].y += icon.width;
+					toMove[i].index += cols;	
+				}
+				else{
+					toMove[i] = {
+						y : icon.y + icon.width,
+						index : i + cols
+					};
+				}
+			}
+		}
+
+		function changeIndexes (){
+			var keys = Object.keys(toMove);
+			if(keys.length == 0) return;
+
+			for(var i = keys.length - 1; i >= 0; i--){
+				var n = keys[i],
+					temp = icons[n],
+					index = toMove[n].index;
+
+				icons[n] = icons[index];
+				icons[index]  = temp;
+			}
+			toMove = {};
+		}
+
+		function addIcons (){
+			for(var i = 0, l = icons.length; i < l; i++){
+				if(!icons[i].visible){
+					var rnd = Math.floor(Math.random() * game.global.iconNames.length),
+					txt = game.global.iconNames[rnd],
+					x = game.global.offsetX + (i % cols) * icons[i].width,
+					y = game.global.offsetY + Math.floor(i / cols) * icons[i].width;
+
+					icons[i].loadTexture(txt);
+					icons[i].reset(x, y); 
+				}
+			}
+		}
+
+		function moveIcons (fn){
+			if(Object.keys(toMove).length == 0) return fn();
+
+			var tweenCallback = (function () {
+				var completed = 0,
+				l = Object.keys(toMove).length;
+
+				return function () {
+					completed++;
+					if(completed == l){
+						fn();
+					}
+				}
+			}());
+
+			for(var i in toMove){
+				var t = game.add.tween(icons[i]).to({ y : toMove[i].y}, 1000);
+				t.onComplete.add(tweenCallback);
+				t.start();
+			}
+		}
+
+		function defaultValues (){
+			for(var i = 0, l = icons.length; i < l; i++){
+				icons[i].inV = false;
+				icons[i].inH = false;
+				icons[i].processed = false;
+			}
+
+			combs = []; 
+		}
+
+		return {
+			makeTurn : function () {
+				findThreesomes();
+				if (combs.length === 0) return;
+
+				combo += 1;
+				computeMovements();
+
+				var self = this;
+
+				moveIcons(function () {
+					changeIndexes();
+					addIcons();
+					defaultValues();
+
+					self.onTurnFinished({
+						points : points, 
+						combo : combo
+					});
+
+					combo = 0;
+					points = 0;
+
+					self.makeTurn();
+				});
+
+			},
+
+			onTurnFinished : (function(){
+				var callback;
+				return function(arg){
+					if(!callback) callback = arg;
+					else{
+						callback(arg);
+					}
+				}
+			}()),
+
+			insertIcon : function (index, txt) {
+				icons[index].loadTexture(txt);
+			}
+		}
 }
 
-playState.Sequence = function (game) {
-	this.game = game;
-	this.elements = [];
+playState.initSequence = function (game) {
+	var elements = [];
 	for(var i = 0, l = game.global.sequenceLength; i < l; i++){
 		var x = game.global.width / 2 - game.global.iconSize * 3 + i * game.global.iconSize,
 			y = game.global.height - game.global.offsetY / 2 - game.global.iconSize / 2,
 			name = game.global.iconNames[Math.floor(Math.random() * game.global.iconNames.length)];
 
-		this.elements[i] = game.add.image(x, y, name);
-		this.elements[i].scale.x = this.elements[i].scale.y = (game.global.iconSize / game.global.imageSize).toFixed(3);
-	}
-}
-
-playState.Sequence.prototype.shift = function(){
-	for(var i = 1, l = this.elements.length; i < l; i++){
-		var txt = this.elements[i].key;
-		this.elements[i - 1].loadTexture(txt);
+		elements[i] = game.add.image(x, y, name);
+		elements[i].scale.x = elements[i].scale.y = (game.global.iconSize / game.global.imageSize).toFixed(3);
 	}
 
-	var name = game.global.iconNames[Math.floor(Math.random() * game.global.iconNames.length)];
-	this.elements[this.elements.length - 1].loadTexture(name);
+	return {
+		shift : function (){
+			for(var i = 1, l = elements.length; i < l; i++){
+				var txt = elements[i].key;
+				elements[i - 1].loadTexture(txt);
+			}
+
+			var name = game.global.iconNames[Math.floor(Math.random() * game.global.iconNames.length)];
+			elements[elements.length - 1].loadTexture(name);
+		},
+
+		getFirstKey : function () {
+			return elements[0].key;
+		}
+	}
 }
